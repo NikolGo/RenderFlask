1f79310be30fc409567d798541f462345d78ef1d
NikolGo <nikolgolberg@gmail.com>
2023-04-26 22:17:15 +0300

    Updating app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
+from flask_socketio import SocketIO, emit
+
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
@@ -13,4 +16,8 @@ def webhook():
         # Handle POST request
         # client.publish("testingProjectBGU", payload="FlaskPOST", qos=0, retain=False)
         print("In POST request")
+        socketio.emit('server_message', 'Message received from Salesforce!')
         return 'This is a POST request', 200
+
+if __name__ == '__main__':
+    socketio.run(app)
