b59e89d2b4473e2af961455175d1b7de0a174588
NikolGo <89397491+NikolGo@users.noreply.github.com>
2023-04-29 13:06:07 +0300

    Removed GET

--- app.py
+++ app.py
@@ -4,20 +4,15 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
     # Process the message payload
-    if request.method == 'GET':
-        # Handle GET request
-        # client.publish("testingProjectBGU", payload="FlaskGET", qos=0, retain=False)
-        print("In GET request")
-        return 'This is a GET request', 200
-    elif request.method == 'POST':
-        # Handle POST request
-        # client.publish("testingProjectBGU", payload="FlaskPOST", qos=0, retain=False)
-        print("In POST request")
-        socketio.emit('server_message', 'Message received from Salesforce!')
-        return 'This is a POST request', 200
+    print("In POST request")
+    data = request.get_json()
+    message = data['Location Id']
+    print(message)
+    socketio.emit('server_message', message)
+    return 'This is a POST request', 200
 
 if __name__ == '__main__':
     socketio.run(app)
