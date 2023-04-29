c7647ee7c914a398dd1e15fedef65b71d938a666
NikolGo <89397491+NikolGo@users.noreply.github.com>
2023-04-29 13:32:39 +0300

    Occupancy_update

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def webhook():
     # Process the message payload
     print("In POST request")
     data = request.get_json()
-    message = data['Location Id']
-    print(message)
-    socketio.emit('server_message', message)
+    # message = data['Location Id']
+    print(data)
+    socketio.emit('occupancy_update', data)
     return 'This is a POST request', 200
 
 if __name__ == '__main__':
